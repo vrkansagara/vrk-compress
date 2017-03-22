3477d83c5fd86749e8727946e76b6b906754c1f9
Vallabh Kansagara <vallabh.kansagara@knowarth.com>
2017-03-22 11:18:59 +0530

    adding psr support

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # vrk-compress
 Compress your WordPress final output.
+-[] Add Admin support
+-[] HTML On/Off Option
+-[] CSS On/Off Option
+-[] JS On/Off Option
+-[] Custom Plugines / theme comment
+
---
+++ autoload.php
@@ -0,0 +1,74 @@
+<?php
+/*
+BSD 3-Clause License
+
+Copyright (c) 2017, Vallabh Kansagara
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/**
+ * Autoloader for the Vrk plugin for WordPress
+ */
+
+/**
+ * Register the autoloader for the Vrk plugin classes.
+ *
+ * Based off the official PSR-4 autoloader example found here:
+ * https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
+ *
+ * @param string $class The fully-qualified class name
+ *
+ * @return void
+ */
+spl_autoload_register(function ($class) {
+    // project-specific namespace prefix
+    $prefix = 'Vrk\\';
+
+    // base directory for the namespace prefix
+    $base_dir = defined('VRK_PLUGIN_DIR') ? VRK_PLUGIN_DIR : __DIR__ . '/src/Vrk/';
+
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (0 !== strncmp($prefix, $class, $len)) {
+        // no, move to the next registered autoloader
+        return;
+    }
+
+    // get the relative class name
+    $relative_class = substr($class, $len);
+
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
