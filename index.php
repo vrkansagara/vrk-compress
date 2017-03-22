1bea904f2a3ebb1bde5caa9cb36ef719f308af7a
Vallabh Kansagara <vrkansagara@gmail.com>
2017-03-23 01:13:51 +0530

    moving js to footer

--- index.php
+++ index.php
@@ -38,3 +38,16 @@ function foo_buffer_stop(){
 
 // Disable W3TC footer comment for all users
 add_filter( 'w3tc_can_print_comment', '__return_false', 10, 1 );
+
+
+function remove_head_scripts() { 
+remove_action('wp_head', 'wp_print_scripts'); 
+remove_action('wp_head', 'wp_print_head_scripts', 9); 
+remove_action('wp_head', 'wp_enqueue_scripts', 1);
+
+add_action('wp_footer', 'wp_print_scripts', 5);
+add_action('wp_footer', 'wp_enqueue_scripts', 5);
+add_action('wp_footer', 'wp_print_head_scripts', 5); 
+} 
+add_action( 'wp_enqueue_scripts', 'remove_head_scripts' );
+
