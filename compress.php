92f77de53a9e34b9880d10d0657dda9ca4753bbb
root <root@vrkansagara.in>
2017-03-26 20:30:32 +0530

    update of plugines name

--- compress.php
+++ compress.php
@@ -2,7 +2,7 @@
 /*
  * Date: 22/3/17
  * Time: 10:40 AM
- * Plugin Name: wp-compressor
+ * Plugin Name: VRK Compresso
  * Plugin URI: https://vrkansagara.in/plugin/wp-compressor
  * Description: Compress all final output
  * Version: 1.0
